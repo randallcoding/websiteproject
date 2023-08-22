b0d6d03290b2efdc275ecf0438484bb4b9f78f86
Alex <alex@linux.myguest.virtualbox.org>
2023-08-22 03:57:46 +0100

    initial setup including sass

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>"Company Site"</div>
+    </div>
+  );
+}
+
+export default App;
